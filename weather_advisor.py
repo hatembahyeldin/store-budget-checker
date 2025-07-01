b3745c1b890f1a6572d74d8fd90954283b7bf970
hatembahyeldin <hatembahyeldin@gmail.com>
2025-07-01 22:47:02 +0300

    Add weather advisor and divisible by 3 tasks

---
+++ weather_advisor.py
@@ -0,0 +1,10 @@
+temp = int(input("Enter the current temperature in Celsius: "))
+
+if temp >= 30:
+    print("It's a hot day. Stay hydrated!")
+elif temp >= 20:
+    print("It's a warm day. Enjoy the weather!")
+elif temp >= 10:
+    print("It's a cool day. Wear a jacket!")
+else:
+    print("It's a cold day. Stay warm!")
